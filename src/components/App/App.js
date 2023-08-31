0bf26c3781ac2a43da6d9ade7b7890859f9d7680
Alexander Gorbatenko <alexander@mailup.net>
2023-08-31 09:00:48 +0300

    Файловая структура

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+import {Route, Routes, useNavigate} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        {/*<Route path='/' element={<Main />} />*/}
+        {/*<Route path='/movies' element={<Movies />} />*/}
+        {/*<Route path='/saved-movies' element={<SavedMovies />} />*/}
+        {/*<Route path='/profile' element={<Profile />} />*/}
+        {/*<Route path='/signin' element={<Login />} />*/}
+        {/*<Route path='/signup' element={<Register />} />*/}
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Main (props) {
+
+  return (
+    <main className="main">
+      <section className="">
+      </section>
+      <section aria-label="" className="">
+      </section>
+    </main>
+  );
+}
+
+export default Main;
