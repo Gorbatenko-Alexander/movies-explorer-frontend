565023a345a552720d433d0fe455d12fa6bc63f6
Alexander Gorbatenko <alexander@mailup.net>
2023-09-30 08:42:42 +0300

    Самостоятельные доработки

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 const EMAIL_VALIDATION_EXP = "^[\\w\\.\\-]+@[\\w\\.\\-]+\\.[\\w]+$";
-const NAME_VALIDATION_EXP = "^[a-zA-Zа-яА-Я\\s\\-]+$";
+const NAME_VALIDATION_EXP = "^[a-zA-Zа-яА-Я\\-][a-zA-Zа-яА-Я\\s\\-]+[a-zA-Zа-яА-Я\\-]$";
 
 module.exports = { EMAIL_VALIDATION_EXP, NAME_VALIDATION_EXP };
