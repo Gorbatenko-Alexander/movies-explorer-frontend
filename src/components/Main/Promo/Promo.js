65446274a2ad950720deeaa0525e99370675abf9
Alexander Gorbatenko <alexander@mailup.net>
2023-09-30 09:13:28 +0300

    Самостоятельные доработки

--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -8,7 +8,12 @@ function Techs () {
       <h1 className="promo__title">Учебный проект студента факультета <nobr>Веб-разработки</nobr>.</h1>
       <p className="promo__description">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
       <button className="promo__button"
-              onClick={() => {window.scrollTo({ top: 648, behavior: 'smooth' });}}
+              onClick={() => {
+                const width = document.documentElement.clientWidth;
+                if (width > 1100) window.scrollTo({ top: 648, behavior: 'smooth' });
+                if ((width <= 1100) && (width > 740)) window.scrollTo({ top: 924, behavior: 'smooth' });
+                if (width <= 740) window.scrollTo({ top: 710, behavior: 'smooth' });
+              }}
       >Узнать больше</button>
     </section>
   );
